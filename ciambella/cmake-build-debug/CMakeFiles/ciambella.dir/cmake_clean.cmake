fa58fe477f6e736340c754295c3a8d049d6d2452
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2022-03-24 21:38:27 +0100

    Add files via upload

---
+++ ciambella/cmake-build-debug/CMakeFiles/ciambella.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ciambella.dir/main.c.obj"
+  "ciambella.exe"
+  "ciambella.exe.manifest"
+  "ciambella.pdb"
+  "libciambella.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ciambella.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
